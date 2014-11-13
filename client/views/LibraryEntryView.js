dd276591272509ca14fdfb4258366c034c759680
cdepman <cdepman@gmail.com>
2014-11-12 20:00:27 -0800

    slowly making progress

--- client/views/LibraryEntryView.js
+++ client/views/LibraryEntryView.js
@@ -3,11 +3,12 @@ var LibraryEntryView = Backbone.View.extend({
 
   tagName: 'tr',
 
-  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
+  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td><td><button class="addToQueue">Add To Queue</button></td>'),
 
   events: {
     'click': function() {
       this.model.play();
+      this.model.enqueue();
     }
   },
 
